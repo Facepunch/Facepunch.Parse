3b3bea7a01ef8978e68e41967a1ad2c6efa02302
James King <james.king@facepunchstudios.com>
2016-08-23 23:52:09 +0100

    Added test project

---
+++ Facepunch.Parse.Test/WhitespaceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Facepunch.Parse.Test
+{
+    [TestClass]
+    public class WhitespaceTest
+    {
+        [TestMethod]
+        public void BasicWhitespace()
+        {
+            var document = new NamedParser( "Document" );
+            var sentence = new NamedParser( "Sentence" );
+            var period = new NamedParser( "Period" );
+            var word = new NamedParser( "Word" );
+
+            using ( ConcatParser.AllowWhitespace( " " ) )
+            {
+                document.Resolve( sentence | (sentence + document) );
+                sentence.Resolve( (word + period) | (word + sentence) );
+                period.Resolve( (Parser) "." | "?" | "!" );
+                word.Resolve( new Regex( "[a-z]+", RegexOptions.IgnoreCase ) );
+            }
+
+            Assert.IsTrue( document.Parse( "Hello world! How are you doing today? This is another sentence. Testing." ).Success );
+            Assert.IsFalse( document.Parse( "Hello world, How are you doing today? This is another sentence. Testing." ).Success );
+            Assert.IsFalse( document.Parse( "Hello world! How are you doing today? This is another sentence. Testing" ).Success );
+        }
+    }
+}
